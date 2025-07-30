e4199c7c7e507dddaab36d41d7f78a6d272819be
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-07-30 15:18:11 +0530

    second problem solved

--- IntroductoryProblems/MissingNumber.cpp
+++ IntroductoryProblems/MissingNumber.cpp
@@ -1,2 +1,28 @@
 // Missing number 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int n;
+     cin >> n;
+
+     vector<int> arr(n-1);
+     for(int i=0; i<n-1; i++){
+         cin >> arr[i];
+     }
+
+     // sort the array 
+     sort(arr.begin() , arr.end());
+     int ans = 0;
+     for(int i=0; i<n-1; i++){
+         if(arr[i]  != (i+1)){
+            ans = i+1;
+            break;
+         }
+     }
+     cout << ans << "/n";
+    
+    return 0;
+
+}
